25eaf09025bb0173a86bbda8db73e927d89b7941
vitalii8554910 <vitalii@inuigaming.com>
2023-03-30 15:34:54 +0400

    feature/NETPOOLZP-5199 Added events for kafka

---
+++ src/main/java/snc/sncmodels/constants/Events.java
@@ -0,0 +1,7 @@
+package snc.sncmodels.constants;
+
+public class Events {
+    public static final String MAKE_MANUAL_BET = "make_manual_bet";
+    public static final String MAKE_AUTOMATIC_BET = "make_manual_bet";
+    public static final String CHANGE_BALANCE = "change_balance";
+}
